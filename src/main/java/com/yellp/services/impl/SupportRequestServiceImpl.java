f053b5318a921a0d3e771cb5f32b6e0b14e71e0e
Srinath-R <r.srinath@live.com>
2018-09-22 17:26:20 +0530

    refactoring.

--- src/main/java/com/yellp/services/impl/SupportRequestServiceImpl.java
+++ src/main/java/com/yellp/services/impl/SupportRequestServiceImpl.java
@@ -6,7 +6,6 @@ import com.yellp.repository.SupportRequestRepository;
 import com.yellp.services.MessagePublisher;
 import com.yellp.services.SupportRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
